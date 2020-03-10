5cf666d9f2a95c098065b5d2a49dea926857d611
cool-library-2371 <est.ariel@gmail.com>
2020-03-10 15:58:09 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -2,24 +2,9 @@
 
 function myAnimal() {
 var animal = 'dog';
+return animal;
 
-
-describe('yourAnimal()', function() {
-    it('returns your animal', function(){
-      expect(window.yourAnimal()).toEqual('cat')
-    })
-
-    it('does not hard-code the answer', function() {
-      expect(window.yourAnimal.toString()).toContain("return animal")
-      expect(window.yourAnimal.toString()).toNotContain("return 'cat'")
-    })
-
-    it('does not change the output of the myAnimal function', () => {
-      expect(window.myAnimal()).toEqual('dog')
-    })
-  })
-//
-  describe('add2(n)', function() {
+describe('add2(n)', function() {
     it('adds two to n', function() {
       const n = Math.floor(Math.random() * 1000)
       expect(window.add2(n)).toEqual(n + 2)
