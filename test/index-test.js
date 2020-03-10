faf9359e858fd77e69467768d1667ee14119191b
cool-library-2371 <est.ariel@gmail.com>
2020-03-10 16:14:02 +0000

    Automatically backed up by Learn

--- test/index-test.js
+++ test/index-test.js
@@ -7,8 +7,7 @@ return animal;
 function yourAnimal() {	function yourAnimal() {
   // How can we make sure that this function	  // How can we make sure that this function
   // and the above function both pass?	  // and the above function both pass?
-  // P.S.: You can't just hard-code 'cat' below	  // P.S.: You can't just hard-code 'cat' below
-  return animal	  return animal
+  return animal
 }
 
 
